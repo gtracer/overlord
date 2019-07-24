32c9224e08564fc65688ed50102d29192c48650f
GaneshaThirumurthi <ganesha.ashoka@gmail.com>
2019-07-24 12:27:06 -0700

    connect to service and refactor

--- pkg/boot/boot.go
+++ pkg/boot/boot.go
@@ -1,12 +1,17 @@
 package boot
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
+	"strings"
+	"time"
 )
 
 const (
@@ -14,41 +19,105 @@ const (
 	script     = "k3.sh"
 	kubeConfig = "/etc/rancher/k3s/k3s.yaml"
 	nodeToken  = "/var/lib/rancher/k3s/server/node-token"
+	endpoint   = "http://51.143.124.178:8080/customeroverlord/overlord/%s"
 )
 
+type Config struct {
+	Kubeconfig string `json:"kubeconfig,omitempty"`
+	Token      string `json:"token,omitempty"`
+	Master     string `json:"master,omitempty"`
+	State      string `json:"state,omitempty"`
+	Message    string `json:"message,omitempty"`
+}
+
 func Boot() error {
+	log.Println("Entering boot..")
 	err := fetchK3(script, url)
 	if err != nil {
 		log.Printf("unable to fetch k3 installation script, error %v", err)
 		return err
 	}
-	token := "K101982131c68f7253bd3199f0b079cea5c514775ec73f7452044d3bfa35c58ab45::node:9f661b02b35db66c63a4f4231b358a28"
-	server := "https://overlord-raspberrypi-agent3:6443"
-	serverName := "overlord-raspberrypi-agent3"
-	//server := "https://10.31.102.42:6443"
-	err = installK3(script, token, server, serverName, true)
+	config := &Config{}
+	for {
+		err = postStatus(config)
+		time.Sleep(10 * time.Second)
+		if err != nil {
+			log.Printf("retrying post status, error: %v", err)
+			continue
+		}
+		log.Printf("current config is: %+v", config)
+		err = installK3(script, config)
+		if err != nil {
+			log.Printf("unable to install k3, error: %v", err)
+			continue
+		}
+	}
+	return nil
+}
+
+func getNodeName() (string, error) {
+	log.Println("Entering getConfig..")
+	node, err := os.Hostname()
 	if err != nil {
-		log.Printf("unable to install k3, error: %v", err)
+		log.Printf("unable to get hostname +%v", err)
+		return "", err
+	}
+	node = strings.ReplaceAll(node, ".", "")
+	node = strings.ReplaceAll(node, "overlord-", "")
+	log.Printf("host name is: %s", node)
+	return node, nil
+}
+
+func postStatus(config *Config) error {
+	nodeName, err := getNodeName()
+	if err != nil {
 		return err
 	}
-
-	config, err := getFileContents(kubeConfig)
+	endpointName := fmt.Sprintf(endpoint, nodeName)
+	sKubeConfig, err := getFileContents(kubeConfig)
 	if err != nil {
 		log.Printf("unable to get kube config, error: %v", err)
 		return err
 	}
-	log.Printf("Kubeconfig contains: %s", config)
+	config.Kubeconfig = sKubeConfig
+	log.Printf("Kubeconfig contains: %s", sKubeConfig)
 
 	sToken, err := getFileContents(nodeToken)
 	if err != nil {
 		log.Printf("unable to get kube config, error: %v", err)
 		return err
 	}
-	log.Printf("Kubeconfig contains: %s", sToken)
+	config.Token = sToken
+	log.Printf("token contains: %s", sToken)
+
+	body, err := json.Marshal(config)
+	if err != nil {
+		log.Printf("unable to marshal body, error %+v", err)
+		return err
+	}
+	resp, err := http.Post(endpointName, "application/json", bytes.NewBuffer(body))
+	if err != nil {
+		log.Printf("unable to get config response +%v", err)
+		return err
+	}
+
+	defer resp.Body.Close()
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("unable to get config body +%v", err)
+		return err
+	}
+
+	err = json.Unmarshal(body, config)
+	if err != nil {
+		log.Printf("unable to unmarshal config +%v", err)
+		return err
+	}
 	return nil
 }
 
 func fetchK3(filename, url string) error {
+	log.Println("Entering fetchk3..")
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Printf("unable to fetch k3 installation script, err %v", err)
@@ -66,7 +135,7 @@ func fetchK3(filename, url string) error {
 	return err
 }
 
-func installK3(script, token, server, serverName string, isMaster bool) error {
+func installK3(script string, config *Config) error {
 	permission := exec.Command("chmod", "+x", script)
 	err := permission.Run()
 	if err != nil {
@@ -76,18 +145,34 @@ func installK3(script, token, server, serverName string, isMaster bool) error {
 
 	log.Printf("installing k3...")
 	var install *exec.Cmd
+	nodeName, err := os.Hostname()
+	if err != nil {
+		log.Printf("unable to get hostname +%v", err)
+		return err
+	}
+	nodeName = strings.ReplaceAll(nodeName, ".", "")
+	var isMaster bool
+
+	if strings.EqualFold(config.Master, nodeName) {
+		isMaster = true
+	}
 	if isMaster {
+		log.Print("bootstrapping master")
 		args := []string{
 			"--write-kubeconfig-mode=664",
-			"--tls-san=" + serverName,
+			"--tls-san=" + config.Master,
 		}
 		install = exec.Command("./"+script, args...)
 	} else {
+		log.Print("bootstrapping agent")
+		url := fmt.Sprintf("http://%s:6443", config.Master)
 		args := []string{
-			"--token=" + token,
-			"--server=" + server,
+			"--token=" + config.Token,
+			"--server=" + url,
 		}
 		install = exec.Command("./"+script, args...)
+		install.Env = append(install.Env, "K3S_TOKEN="+config.Token)
+		install.Env = append(install.Env, "K3S_URL="+url)
 	}
 	err = install.Run()
 	if err != nil {
@@ -104,10 +189,16 @@ func installK3(script, token, server, serverName string, isMaster bool) error {
 			return err
 		}
 	}
+	config.State = "Healthy"
+	config.Message = "successfully bootstrapped node"
 	return nil
 }
 
 func getFileContents(path string) (string, error) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return "", nil
+	}
+
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
 		log.Printf("unable to read file %s, error: %+v", path, err)
@@ -115,9 +206,3 @@ func getFileContents(path string) (string, error) {
 	}
 	return string(data), nil
 }
-
-/*
-Kubeconfig (master only)
-Node-token (master only)
-Status: healthy/unhealthy
-*/
